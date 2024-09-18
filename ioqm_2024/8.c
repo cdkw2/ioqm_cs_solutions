327d17e8cf4c999f63174525ef6632de2f8620c8
cdkw2 <67598073+cdkw2@users.noreply.github.com>
2024-09-18 23:09:54 +0530

    added problem 8

---
+++ ioqm_2024/8.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int dsum(int n){
+	int dig = 0, dsum = 0;
+	while(n>0){
+		dig = n%10;
+		dsum+=dig;
+		n=n/10;
+	}
+	return(dsum);
+}
+
+int main(){
+	int n = 10, number;
+	while(1){
+		n+=1;
+		if(dsum(n)%5 == 0 && dsum(n+1)%5 == 0){
+			number = n;
+			break;
+		}
+	}
+	printf("%d", number);
+
+	return 0;
+}
